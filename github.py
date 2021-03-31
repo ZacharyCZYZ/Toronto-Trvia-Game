0d906248d2acb6126b4de32387d751632f685aed
ZacharyCZYZ <Z.collison@vatcan.ca>
2021-03-31 16:51:02 -0400

    Add files via upload

---
+++ github.py
@@ -0,0 +1,29 @@
+print ("hello, welcome to toronto triva!")
+ans = input('are you ready to play (yes/no) : ')
+score = 0
+total_q=4
+if ans.lower() == "yes":
+    ans = input ("1. how many runways does toronto have?")
+    if ans == "4":
+        Score +=1
+    print("correct")
+else:
+    print ("incorrect")
+ans = input ("2. what is the departure initial attidude?")
+if ans == "5000":
+        score +=1
+print("correct")
+Else
+print("incorrect")
+ans = input ("3. does VAITS do what we want?")
+if ans.lower () == "yes":
+        score +=1
+print("correct")
+Else
+print("incorrect")
+ans = input ("4. what is the max crosswind for a dry runway? (just the number)")
+if ans () == "15" :
+        score +=1
+print("correct")
+Else
+print("incorrect")
