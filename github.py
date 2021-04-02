c4a07e9f9622daa69cf9834602287fc92baf896c
ZacharyCZYZ <Z.collison@vatcan.ca>
2021-04-01 23:07:19 -0400

    Add files via upload

--- github.py
+++ github.py
@@ -2,28 +2,37 @@ print ("hello, welcome to toronto triva!")
 ans = input('are you ready to play (yes/no) : ')
 score = 0
 total_q=4
-if ans.lower() == "yes":
-    ans = input ("1. how many runways does toronto have?")
-    if ans == "4":
-        Score +=1
-    print("correct")
-else:
-    print ("incorrect")
-ans = input ("2. what is the departure initial attidude?")
-if ans == "5000":
-        score +=1
-print("correct")
-Else
-print("incorrect")
-ans = input ("3. does VAITS do what we want?")
 if ans.lower () == "yes":
-        score +=1
-print("correct")
-Else
-print("incorrect")
-ans = input ("4. what is the max crosswind for a dry runway? (just the number)")
-if ans () == "15" :
-        score +=1
-print("correct")
-Else
-print("incorrect")
+    
+    ans=input ("1# how manny runways does toronto have?")
+    if ans.lower () == "5":
+        score += 1
+        print ("Correct")
+    else:
+        print("incorrect")
+
+    ans=input ("2# what is the initial altitude on departure?")
+    if ans.lower () == "5000":
+        score += 1
+        print ("Correct")
+    else:
+        print("Incorrect")
+
+    ans=input ("3# do you turn on course after departure? ")
+    if ans.lower () == "no" :
+        score += 1
+        print ("Correct")
+    else: 
+        print("Incorrect")
+
+    ans=input ("#4 what is the circuit altitude?")
+    if ans.lower () == "1500" :
+        score += 1
+        print ("correct")
+    else:
+        print("Incorrect")
+
+print (" Thank you for playing, you got ",score,"questions correct." )
+mark = (score/total_q) *100
+print ("mark: ", str(mark) + "%" )
+print("Goodbye")
